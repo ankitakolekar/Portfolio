20ba5d9c72ac79c78abdd83bed2b7d0c0c79e245
ankitakolekar <89743252+ankitakolekar@users.noreply.github.com>
2024-04-13 18:37:57 +0530

    Update

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const body = document.querySelector("body");
 const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
+const navText = document.querySelectorAll(".navText");
 menuBtn.onclick = () => {
     navbar.classList.add("show");
     menuBtn.classList.add("hide");
     body.classList.add("disabled");
 }
 cancelBtn.onclick = () => {
+    body.classList.remove("disabled");
+    navbar.classList.remove("show");
+    menuBtn.classList.remove("hide");
+}
+navText.onclick = () => {
     body.classList.remove("disabled");
     navbar.classList.remove("show");
     menuBtn.classList.remove("hide");
